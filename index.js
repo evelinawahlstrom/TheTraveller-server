c136f9b22f1ca9fcc1cf0e345b09ebd75ca7b4bd
Evelina <evelinawahlstrom18@gmail.com>
2019-12-25 11:57:01 +0100

    signup-router-model

---
+++ index.js
@@ -0,0 +1,29 @@
+// Requirements
+const express = require('express')
+const db = require('./db')
+
+// Routers
+const userRouter = require('./user/router')
+
+// Middlewares
+const bodyParser = require('body-parser')
+const bodyParserMiddleWare = bodyParser.json()
+// Connecting with frontend
+const cors = require('cors')
+const corsMiddleWare = cors()
+
+const app = express()
+const port = process.env.PORT || 4000
+
+app
+    .use(corsMiddleWare)
+    .use(bodyParserMiddleWare)
+    .use(userRouter)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// force: true as long as development is in process
+db
+.sync({force: true})
+.then(() => console.log("Database schema updated"))
+.catch(console.error)
